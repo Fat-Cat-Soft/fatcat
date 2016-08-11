644957e909ac36278d03fe824148adb4e369ceda
Nikolay Cherniienko <nikolay.cherniienko@gmail.com>
2016-08-11 14:30:14 +0300

    Replace old files with new

--- www/README.md
+++
@@ -1,4 +0,0 @@
-# fatcat
-
-to start poject, need to install _bower_ - http://bower.io/#install-bower.<br>
-After install bower - need to run bower install from root path.
---
+++ www/js/learn.js
@@ -0,0 +1,13 @@
+function FirstFunction() {
+			document.getElementById("custom-class").innerHTML = "This is my first function!";
+		};
+		
+function calculate(){
+			window.alert(10+15);
+			};
+
+function calculate2(){
+			document.write(10+15);
+			};
+
+			
